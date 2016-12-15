8b3459116caea3072211753af8595be738fc0dfe
DireCorgi <s.frank.ye@gmail.com>
2016-12-15 16:34:58 -0500

    add footer to splash page

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -13,6 +13,14 @@ class SplashPage extends React.Component {
       <section className="splash-page">
         <img src={ window.splashBackgroundPath } alt="background" className={imgClass}/>
         <h2 className={textClass}>Pixel perfect project management</h2>
+
+        <footer className="splash-page-footer group">
+          <h1 className="header-logo splash-footer-logo">
+            <img src={window.lightLogoPath} alt="Logo" />
+            PixelTracker
+          </h1>
+          <a href="https://github.com/DireCorgi/pixelTracker">Github</a>
+        </footer>
       </section>
     );
   }
