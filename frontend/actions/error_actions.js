70f24cb762eb40bbd5764a218973bd40a6bde6c6
DireCorgi <s.frank.ye@gmail.com>
2016-12-15 12:30:32 -0500

    add error actions

---
+++ frontend/actions/error_actions.js
@@ -0,0 +1,6 @@
+export const DRAG_ERROR = 'DRAG_ERROR';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+export const dragAndDropError = () => ({ type: DRAG_ERROR });
+
+export const clearDragAndDropError = () => ({ type: CLEAR_ERRORS });
