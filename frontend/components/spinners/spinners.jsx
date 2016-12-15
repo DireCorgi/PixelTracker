239aaf00d7cbef34d0baf813460bca48b113b32f
DireCorgi <s.frank.ye@gmail.com>
2016-12-15 09:38:47 -0500

    refactor spinner css to be more semantic

--- frontend/components/spinners/spinners.jsx
+++ frontend/components/spinners/spinners.jsx
@@ -16,19 +16,19 @@ export const Spinner = () => {
 
 export const Spinner2 = () => {
   return (
-    <div className="loader-2">Loading...</div>
+    <div className="loader loader-2">Loading...</div>
   );
 };
 
 export const Spinner3 = () => {
   return (
-    <div className="loader-3">Loading...</div>
+    <div className="loader loader-3">Loading...</div>
   );
 };
 
 export const Spinner4 = () => {
   return (
-    <div className="loader-4">Loading...</div>
+    <div className="loader loader-4">Loading...</div>
   );
 };
 
