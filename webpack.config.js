1bb191018bff0d914c47e7cd379639ee53a8c8d7
DireCorgi <s.frank.ye@gmail.com>
2016-12-15 17:03:07 -0500

    try updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,16 @@ module.exports = {
   resolve: {
     extensions: ["", ".js", ".jsx" ]
   },
+  plugins:[
+   new webpack.DefinePlugin({
+     'process.env':{
+       'NODE_ENV': JSON.stringify('production')
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin({
+     compress:{
+       warnings: true
+     }
+   })
+ ]
 };
