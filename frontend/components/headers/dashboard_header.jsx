77c7760986a6e4b2b4c4900e62990a5b7eb23d94
DireCorgi <s.frank.ye@gmail.com>
2016-12-19 17:53:00 -0500

    minor fixes

--- frontend/components/headers/dashboard_header.jsx
+++ frontend/components/headers/dashboard_header.jsx
@@ -58,8 +58,6 @@ class DashboardHeader extends React.Component {
       errorsHeader = <ErrorsHeader clearErrors={this.props.clearErrors}/>;
     }
 
-    const callback = () => console.log("WORKS!");
-
     return(
       <header className="application-header dashboard-header group">
         <h1 className="header-logo-light">
